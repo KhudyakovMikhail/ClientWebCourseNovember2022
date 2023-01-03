37355fa60e2ae54799e20cbe961409321f6cfcef
KhudyakovMikhail <108936536+KhudyakovMikhail@users.noreply.github.com>
2023-01-03 14:55:26 +0700

    Внес изменения в задачу ArraysWork

--- ArraysWork/ArraysWork.js
+++ ArraysWork/ArraysWork.js
@@ -1,59 +1,62 @@
-var array1 = [17, 5, -10, 14, -8, 37, 26, -1, 33, -18, -25, 45, 22, 44];
-console.log(array1);
+(function () {
+    var array1 = [17, 5, -10, 14, -8, 37, 26, -1, 33, -18, -25, 45, 22, 44];
 
-function getSortedArray(array) {
-    return array.sort(function (e1, e2) {
-        return e2 - e1;
-    });
-}
+    function sortArrayDescending(array) {
+        array.sort(function (e1, e2) {
+            return e2 - e1;
+        });
+    }
 
-var sortedArray = getSortedArray(array1)
-console.log(sortedArray);
+    sortArrayDescending(array1)
+    console.log("Отсортированный по убыванию массив чисел:");
+    console.log(array1);
 
-function getFirst5Elements(array) {
-    return array.slice(0, 5);
-}
+    function getFirst5Elements(array) {
+        return array.slice(0, 5);
+    }
 
-var first5Array = getFirst5Elements(sortedArray);
-console.log(first5Array);
+    var first5ArrayElements = getFirst5Elements(array1);
+    console.log("Первые 5 элементов массива:");
+    console.log(first5ArrayElements);
 
-function getLast5Elements(array) {
-    return array.slice(sortedArray.length - 5);
-}
+    function getLast5Elements(array) {
+        return array.slice(array1.length - 5);
+    }
 
-var last5Array = getLast5Elements(sortedArray);
-console.log(last5Array);
+    var last5ArrayElements = getLast5Elements(array1);
+    console.log("Последние 5 элементов массива:");
+    console.log(last5ArrayElements);
 
-function getEvenNumbersSum(array) {
-    return array.filter(function (element) {
-        if (element % 2 === 0) {
-            return element;
-        }
-    }).reduce(function (sum, element) {
-        return sum + element;
-    }, 0);
-}
+    function getEvenNumbersSum(array) {
+        return array.filter(function (element) {
+            return element % 2 === 0;
+        }).reduce(function (sum, element) {
+            return sum + element;
+        }, 0);
+    }
 
-var evenNumbersSum = getEvenNumbersSum(array1);
-console.log(evenNumbersSum);
+    var evenNumbersSum = getEvenNumbersSum(array1);
+    console.log("Сума четных элементов массива:");
+    console.log(evenNumbersSum);
 
-var array2 = [];
+    var array2 = [];
 
-for (var i = 1; i <= 100; i++) {
-    array2.push(i);
-}
+    for (var i = 1; i <= 100; i++) {
+        array2.push(i);
+    }
 
-console.log(array2);
+    console.log("Массив от 1 до 100:");
+    console.log(array2);
 
-function getEvenNumbersSquaresArray(array) {
-    return array.filter(function (element) {
-        if (element % 2 === 0) {
-            return element;
-        }
-    }).map(function (element) {
-        return element * element;
-    });
-}
+    function getEvenNumbersSquaresArray(array) {
+        return array.filter(function (element) {
+            return element % 2 === 0;
+        }).map(function (element) {
+            return element * element;
+        });
+    }
 
-var squaresArray = getEvenNumbersSquaresArray(array2);
-console.log(squaresArray);
+    var squaresArray = getEvenNumbersSquaresArray(array2);
+    console.log("Список квадратов четных чисел массива:");
+    console.log(squaresArray);
+})();
